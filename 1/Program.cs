b16d6286abe0a8e1e6832a8361ebdd7090f8f88a
Matias Ramsland <matiaslramsland01@gmail.com>
2022-12-01 09:54:42 +0100

    day 1

---
+++ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] lines = System.IO.File.ReadAllLines(@"Calories.txt");
+var max = 0;
+var listOfValues = new List<int>();
+var currentValue = 0;
+foreach (string line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if (currentValue > max)
+        {
+            max = currentValue;
+        }
+        listOfValues.Add(currentValue);
+        currentValue = 0;
+    }
+    else { currentValue += Int32.Parse(line); }
+}
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine($"The elf with maximum calories have: {max} kcal");
+Console.WriteLine($"The 3 elf with the highest calories have in total: {listOfValues.OrderByDescending(d => d).Take(3).Sum()} kcal");
