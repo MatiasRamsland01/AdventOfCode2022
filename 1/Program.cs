e33e22fb6f8f9121813b3d24805d27ee7073fb94
Matias Ramsland <matiaslramsland01@gmail.com>
2022-12-02 09:21:10 +0100

    Day 2

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,15 +1,15 @@
 ﻿string[] lines = System.IO.File.ReadAllLines(@"Calories.txt");
-var listOfValues = new List<int>();
+var caloriesCarried = new List<int>();
 var currentValue = 0;
 foreach (string line in lines)
 {
     if (string.IsNullOrWhiteSpace(line))
     {
-        listOfValues.Add(currentValue);
+        caloriesCarried.Add(currentValue);
         currentValue = 0;
     }
     else { currentValue += Int32.Parse(line); }
 }
 Console.BackgroundColor = ConsoleColor.Blue;
-Console.WriteLine($"The elf with maximum calories have: {listOfValues.Max()} kcal");
-Console.WriteLine($"The 3 elf with the highest calories have in total: {listOfValues.OrderByDescending(d => d).Take(3).Sum()} kcal");
+Console.WriteLine($"The elf with maximum calories have: {caloriesCarried.Max()} kcal");
+Console.WriteLine($"The 3 elf with the highest calories have in total: {caloriesCarried.OrderByDescending(d => d).Take(3).Sum()} kcal");
