65edbbba34b8c551fa66ace9c039dbc63208d02e
Matias Ramsland <matiaslramsland01@gmail.com>
2022-12-02 08:57:41 +0100

    Day 2

---
+++ 2/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+var possibilities = new Dictionary<string, int>()
+{
+    {"AX", 4},
+    {"BY", 5},
+    {"CZ", 6},
+    {"AY", 8},
+    {"AZ", 3},
+    {"BZ", 9},
+    {"BX", 1},
+    {"CX", 7},
+    {"CY", 2},
+
+};
+var mappingDict = new Dictionary<string, string>()
+{
+    {"AX", "AZ"},
+    {"BY", "BY"},
+    {"CZ", "CX"},
+    {"AY", "AX"},
+    {"AZ", "AY"},
+    {"BZ", "BZ"},
+    {"BX", "BX"},
+    {"CX", "CY"},
+    {"CY", "CZ"},
+
+};
+var score = 0;
+var scoreCondition = 0;
+foreach (string line in lines)
+{
+    score += possibilities[line.Replace(" ", string.Empty)];
+    scoreCondition += possibilities[mappingDict[line.Replace(" ", string.Empty)]];
+}
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine($"Score of following the plan of direct choosing rock, paper or scissor: {score}");
+Console.WriteLine($"Score of following the plan of either winning, loosing or drawing: {scoreCondition}");
