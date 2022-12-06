4c4e4511ce2213d0c092a5c538ba4f9268f21d54
Matias Ramsland <matiaslramsland01@gmail.com>
2022-12-06 09:09:29 +0100

    day 6

---
+++ 6/Program.cs
@@ -0,0 +1,22 @@
+﻿string lines = System.IO.File.ReadAllText(@"input.txt");
+Queue<string> set = new Queue<string>(); //Using a FIFO queue since we just want to add and remove from the front of the queue.
+var distinctCharacters = 14; //It is 4 in part 1 and 14 in part 2
+var charactersProcessed = 0;
+for (int i = 0; i < lines.Length; i++)
+{
+    set.Enqueue(lines.Substring(i, 1).ToString());
+    if (set.Count() >= distinctCharacters)
+    {
+        var result = set.Distinct().Count() == set.Count(); //Check if all characters are distinct
+        if (result)
+        {
+            charactersProcessed += i + 1;
+            break;
+        }
+        set.Dequeue();
+    }
+}
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine();
+Console.WriteLine($"Characters processed to find start-of-packet marker: {charactersProcessed}");
+
