a74177cc1a4905b1b89e1b8d3aeed1d2cafad705
MatiasRamsland <259150@uis.no>
2022-12-15 10:44:50 +0100

    progress day 15

--- 15/Program.cs
+++ 15/Program.cs
@@ -9,6 +9,7 @@ foreach (var line in lines)
     var match = coords.Matches(line);
     (int sx, int sy) = (int.Parse(match[0].Groups[1].Value), int.Parse(match[0].Groups[2].Value));
     (int bx, int by) = (int.Parse(match[1].Groups[1].Value), int.Parse(match[1].Groups[2].Value));
+    Console.WriteLine($"Start: {sx}, {sy} - Beacon: {bx}, {by}");
     var distance = Math.Abs(sx - bx) + Math.Abs(sy - by);
     var offset = distance - Math.Abs(sy - Y);
     //If offset is smaller it means that offset is smaller than the row difference. This means that all beacons within this row are further away
